5ad3b9b5901835b51b1b98c9c0b4aca136f6ec76
Diode-exe <DiodesCAER@protonmail.com>
2026-01-22 11:43:43 -0600

    this didn't make a difference, my dataset is too small.

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ FADE_DELAY = 0.5
 
 MODEL_PATH = os.path.join("models", "face_recognizer.yml")
 LABELS_PATH = os.path.join("models", "labels.json")
-RECOGNITION_CONF_THRESHOLD = 70
+RECOGNITION_CONF_THRESHOLD = 30
 
 global fadedIn
 
